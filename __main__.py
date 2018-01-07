340bdbb1a5869eb34e627d18f248953202d9f837
Mehow <Kaiser.mehow@gmail.com>
2018-01-07 20:58:52 +0000

    using known x limits to simplify explore algorithm

--- __main__.py
+++ __main__.py
@@ -7,24 +7,20 @@ leftEdge = 475136
 rightEdge = 567295
 imageSize = 513
 
-x_limits = {
-    "left": math.floor(leftEdge / imageSize),
-    "right": math.ceil(rightEdge / imageSize)
-}
+x_min = int(math.floor(leftEdge / imageSize))
+x_max = int(math.ceil(rightEdge / imageSize))
 
 # y doesnt seem to have a limit
 # presumably floor serves as a limit on downwards
 # use y start cord initially
 
-y_limits = {
-    "top": math.ceil(549612 / imageSize),
-    "bottom": math.floor(549612 / imageSize)
-}
+y_start = int(549612 / imageSize)
 
-#x_limits, y_limits = pull_images.pull_all_images(x_limits, y_limits)
+x_limits, y_limits = pull_images.pull_all_images(x_min, x_max, y_start)
 
-# these were taken from xkcd_map's output
-x_limits = {
+# these were taken from pull_all_images output
+# uncomment if the tiles have been downloaded in a previous run
+"""x_limits = {
     "left": 928,
     "right": 1074
 }
@@ -32,7 +28,7 @@ x_limits = {
 y_limits = {
     "top": 1112,
     "bottom": 1068
-}
+}"""
 
 stitch_images.stitch_tiles_to_rows(y_limits, x_limits)
 stitch_images.stitch_rows_to_image(x_limits, y_limits)
--- pull_images.py
+++ pull_images.py
@@ -5,64 +5,48 @@ img_dir = 'pulled_images/'
 
 
 def get_tile(x, y):
-    # save as y_x so grid view of directory matches cords
-    # in os gui grid (sorted alphabetically)
+    # save as y_x so it looks right sorted alphabetically in directories
     url = "http://xkcd.com/1608/" + str(x) + ":-" + str(y) + "+s.png"
     response = requests.get(url, stream=True)
     file_name = str(y) + "_" + str(x)
-    print("x: " + str(x) + " y: " + str(y))
     # empty tiles (i.e. all white) get 404's
     if response.status_code == 200:
-        print("got 200")
         file_path = img_dir + file_name + ".png"
-        with open(file_path, 'wb') as out_file:
+        with open(file_path, 'wb+') as out_file:
             shutil.copyfileobj(response.raw, out_file)
     return response.status_code
 
 
-def expand_limit(position, change, perpendicular_limits, explore_axis):
-    while True:
-        all404 = True
-        for limit_axis in range(perpendicular_limits["least"], perpendicular_limits["most"]):
-            if explore_axis == "x":
-                status_code = get_tile(position, limit_axis)
-            else:
-                status_code = get_tile(limit_axis, position)
-            if status_code != 404:
-                all404 = False
-        if all404:
-            return position
-        else:
-            position += change
+def row_is_all_404(x_min, x_max, y):
+    all_404 = True
+    for x in range(x_min, x_max + 1):
+        if get_tile(x, y) != 404:
+            all_404 = False
+    return all_404
 
 
-def explore_axis(expanding_limits, hard_limits, expanding_axis):
-    print("exploring most")
-    new_most = expand_limit(expanding_limits["most"], 1, hard_limits, expanding_axis)
-    print("exploring least")
-    new_least = expand_limit(expanding_limits["least"], -1, hard_limits, expanding_axis)
-    if new_most == expanding_limits["most"] and new_least == expanding_limits["least"]:
-        return False
-    else:
-        expanding_limits["most"] = new_most
-        expanding_limits["least"] = new_least
-        return True
-
-
-def pull_all_images(x_limits, y_limits):
+def pull_all_images(x_min, x_max, y_start):
     """Initial strategy was to expand along alternating axis
     Changing as each axis got only 404s for a row/column
     This is likely unnecessary, as limits for the x where pulled from the source
     Instead, we could of just expanded along the y until all 404s"""
-    print("y explore")
-    explore_axis(y_limits, x_limits, "y")
-    while True:
-        print("x explore")
-        if not explore_axis(x_limits, y_limits, "x"):
-            break
-        print("y explore")
-        if not explore_axis(y_limits, x_limits, "y"):
-            break
-    print("explored from x: " + str(x_limits["least"]) + " y: " + str(y_limits["least"]) +
-          "to x: " + str(x_limits["most"]) + " y: " + str(y_limits["least"]))
-    return x_limits, y_limits
+    y = y_start
+    print("Exploring up")
+    while not row_is_all_404(x_min, x_max, y):
+        y += 1
+        if y%10 == 0:
+            print("Row " + str(y) + " done")
+    top = y
+    print("Exploring down")
+    y = y_start
+    while not row_is_all_404(x_min, x_max, y):
+        y -= 1
+        if y%10 == 0:
+            print("Row " + str(y) + " done")
+    bottom = y
+
+    y_limits = {
+        "bottom": bottom,
+        "top": top
+    }
+    return {"left": x_min, "right": x_max}, y_limits
